230b5975988f92b9c720e56e028fc25ebf1e056b
danialhx <hussin.danial@gmail.com>
2022-03-16 14:27:15 +0800

    Add files via upload
    108-115 Cloud9 IDE AWS reStart

---
+++ Fruit.py
@@ -0,0 +1,31 @@
+print("List")
+myFruitList = ["apple","bannana","watermelon"]
+print(myFruitList)
+print(type(myFruitList))
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+myFruitList[2] = "orange"
+print(myFruitList)
+print("")
+
+print("Tuple")
+myFinalAnswerTuple = ("apple","bannana","pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+print("")
+
+print("Dictionary")
+myFavouriteFruitDictionary = {
+    "Akua" : "apple",
+    "Saanvi" : "bannana",
+    "Paulo" : "pineapple",
+}
+print(myFavouriteFruitDictionary)
+print(type(myFavouriteFruitDictionary))
+print(myFavouriteFruitDictionary["Akua"])
+print(myFavouriteFruitDictionary["Saanvi"])
+print(myFavouriteFruitDictionary["Paulo"])
---
+++ fLoop.py
@@ -0,0 +1,3 @@
+myMixedTypedList = [45, 290578, 1.02, True, "My dog is on the bed.", "45"]
+for item in myMixedTypedList:
+    print("{} is of the data type{}".format(item,type(item)))
